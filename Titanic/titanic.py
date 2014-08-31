5845009adcd83ea64850920c71542396f5575f87
Eric Randall <eric.randalls@gmail.com>
2014-08-31 11:38:04 -0400

    Following Titanic tutorial, v1

--- Titanic/titanic.py
+++ Titanic/titanic.py
@@ -1,13 +1,35 @@
 import csv as csv 
 import numpy as np
 
-csv_file_object = csv.reader(open('train.csv', 'rb')) 
-header = csv_file_object.next()  # The next() command just skips the 
-                                 # first line which is a header
-data=[]                          # Create a variable called 'data'.
-for row in csv_file_object:      # Run through each row in the csv file,
-    data.append(row)             # adding each row to the data variable
-data = np.array(data) 	         # Then convert from a list to an array
-			         # Be aware that each item is currently
-                                 # a string in this format
-print data
+csv_file_object = csv.reader(open('data/train.csv', 'rb')) 
+header = csv_file_object.next()
+
+data=[]
+
+for row in csv_file_object:
+    data.append(row)
+data = np.array(data)
+
+#print data[0::,4] #0:: means all data
+#print data[0::,2].astype(np.float)
+
+number_passengers = np.size(data[0::,1].astype(np.float))
+number_survived = np.sum(data[0::,1].astype(np.float))
+proportion_survivors = number_survived / number_passengers
+print 'Total amount of passengers was %s' % number_passengers
+print 'Total amount of survivors %s' % number_survived
+print 'Total Titanic survival rate is %s' % proportion_survivors
+
+women_only_stats = data[0::,4] == "female" #num. of females
+men_only_stats = data[0::,4] != "female" #num. of males
+
+# Using the index from above we select the females and males separately
+women_onboard = data[women_only_stats,1].astype(np.float)     
+men_onboard = data[men_only_stats,1].astype(np.float)
+
+# Then we finds the proportions of them that survived
+proportion_women_survived = np.sum(women_onboard) / np.size(women_onboard)  
+proportion_men_survived = np.sum(men_onboard) / np.size(men_onboard) 
+
+print 'Proportion of women who survived is %s' % proportion_women_survived
+print 'Proportion of men who survived is %s' % proportion_men_survived
