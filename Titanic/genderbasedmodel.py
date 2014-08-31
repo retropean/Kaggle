6686bc3c26ef44b7aa51be5e99449557dca14cbf
Eric Randall <eric.randalls@gmail.com>
2014-08-31 11:49:29 -0400

    Create 'Gender Based Model'

---
+++ Titanic/genderbasedmodel.py
@@ -0,0 +1,18 @@
+import csv as csv 
+import numpy as np
+
+test_file = open('data/test.csv', 'rb')
+test_file_object = csv.reader(test_file)
+header = test_file_object.next()
+
+prediction_file = open("data/genderbasedmodel.csv", "wb")
+prediction_file_object = csv.writer(prediction_file)
+
+prediction_file_object.writerow(["PassengerId", "Survived"])
+for row in test_file_object:
+    if row[3] == 'female':                                    
+        prediction_file_object.writerow([row[0],'1'])
+    else:
+        prediction_file_object.writerow([row[0],'0'])
+test_file.close()
+prediction_file.close()
