358a4968bccd5b9ce3b69169fd35ccfd4adba754
Eric Randall <eric.randalls@gmail.com>
2014-09-10 10:25:29 -0400

    Finished tutorial on Gender Class Model
    Finished tutorial on Gender Class Model (genderclassmodel.py) and
    renamed some of the files to be in line with the Kaggle tutorial files.

--- Titanic/genderclassmodel.py
+++ Titanic/genderclassmodel.py
@@ -34,9 +34,39 @@ for i in xrange(number_of_classes):
                                  & (data[0:,9].astype(np.float) >= j*fare_bracket_size) \
                                  & (data[0:,9].astype(np.float) < (j+1)*fare_bracket_size), 1]
 
-                                 #if i == 0 and j == 3:
-
         survival_table[0,i,j] = np.mean(women_only_stats.astype(np.float))  # Female stats
         survival_table[1,i,j] = np.mean(men_only_stats.astype(np.float))    # Male stats
+
 survival_table[ survival_table != survival_table ] = 0.
+
+survival_table[ survival_table < 0.5 ] = 0
+survival_table[ survival_table >= 0.5 ] = 1
+
 print survival_table
+
+test_file = open('data/test.csv', 'rb')
+test_file_object = csv.reader(test_file)
+header = test_file_object.next()
+predictions_file = open("data/genderclassmodel.csv", "wb")
+p = csv.writer(predictions_file)
+p.writerow(["PassengerId", "Survived"])
+
+for row in test_file_object:
+	for j in xrange(number_of_price_brackets):
+		try:
+			row[8] = float(row[8])
+		except:
+			bin_fare = 3 - float(row[1])
+			break
+		if row[8] > fare_ceiling:
+			bin_fare = number_of_price_brackets-1
+			break
+		if row[8] >= j * fare_bracket_size\
+			and row[8] < (j+1) * fare_bracket_size:
+			bin_fare = j
+			break
+	if row[3] == 'female':p.writerow([row[0], "%d" % int(survival_table[0, float(row[1])-1, bin_fare])])
+	else: p.writerow([row[0], "%d" % int(survival_table[1, float(row[1])-1, bin_fare])])
+
+test_file.close() 
+predictions_file.close()
