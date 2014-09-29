5b1191847c9657f39389d5ea555ea1bd29aef6f6
Eric Randall <eric.randalls@gmail.com>
2014-09-29 15:51:54 -0400

    Rush Hour Indicator
    Slightly ghetto mapping of rush hour indicator... but it works

--- Bike Sharing Demand/bikesharingdemandvariables.py
+++ Bike Sharing Demand/bikesharingdemandvariables.py
@@ -8,19 +8,16 @@ from sklearn.ensemble import RandomForestClassifier
 df = pd.read_csv('data/train.csv', header=0)
 
 #Create variable for each hour of the day
-df['hour'] = df['datetime']
-df['hour'] = df.hour.apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S').hour)
+df['hour'] = df.datetime.apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S').hour)
 
 #Create variable for DOW
-df['dow'] = df['datetime']
-df['dow'] = df.dow.apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S').isoweekday())
+df['dow'] = df.datetime.apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S').isoweekday())
 
 #Create variable for Month
-df['month'] = df['datetime']
-df['month'] = df.month.apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S').month)
+df['month'] = df.datetime.apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S').month)
 
 #Create indicator for Rush Hour
-
+df['rushhour'] = df['hour'].map({0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 1, 7: 1, 8: 1, 9: 1, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 1, 16: 1, 17: 1, 18: 1, 19: 0, 20: 0, 21: 0, 22: 0, 23: 0, 24: 0})
 
 #print df.mean()
 print df.describe()
