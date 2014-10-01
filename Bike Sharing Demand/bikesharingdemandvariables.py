d5842269fa985917b993f5314bc533bdf8d0eebf
Eric Randall <eric.randalls@gmail.com>
2014-10-01 16:08:34 -0400

    Sunrise/Sunset indicator Fix
    Now sunrise and sunset are calculated with a half hour grace period.

--- Bike Sharing Demand/bikesharingdemandvariables.py
+++ Bike Sharing Demand/bikesharingdemandvariables.py
@@ -3,6 +3,7 @@ import pandas as pd
 import pylab as P
 import csv as csv
 from datetime import datetime
+from datetime import timedelta
 from sklearn.ensemble import RandomForestClassifier
 
 df = pd.read_csv('data/train.csv', header=0)
@@ -14,32 +15,47 @@ df['hour'] = df['datetime'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M
 df['year'] = df['datetime'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S').year)
 df['dow'] = df['datetime'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S').isoweekday())
 df['month'] = df['datetime'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S').month)
+df['dateandtime'] = df['datetime'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
 df['date'] = df['datetime'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S').strftime('%m/%d/%Y'))
 
 #create indicator for rush hour, 6 to 9am and 3 to 6pm
 df['rushhour'] = np.where(((df['hour']>=6) & (df['hour'] <=9)) | ((df['hour']>=15) & (df['hour'] <=18)),1,0)
 
-#adjust date on sundata, drop unneccesary rows
-sundata['date'] = sundata['date'].apply(lambda x: datetime.strptime(x, '%m/%d/%Y').strftime('%m/%d/%Y'))
-
+#adjust dates on sundata, drop unneccesary rows
+sundata['date'] = sundata['date'].apply(lambda x: datetime.strptime(x, '%m/%d/%Y'))
 sundata['sunrise'] = sundata['sunrise'].apply(lambda x: datetime.strptime(str(x), "%H%M").time())
 sundata['sunset'] = sundata['sunset'].apply(lambda x: datetime.strptime(str(x), "%H%M").time())
-#sundata['sunset'] = (sundata['sunset'],sundata['date']).apply(lambda x,y: datetime.combine(x, y))
+
+def sunrisecombine(x):
+	return (datetime.combine(x['date'], x['sunrise']))
+
+def sunsetcombine(x):
+	return (datetime.combine(x['date'], x['sunset']))	
+
+sundata['sunrise'] = sundata.apply(sunrisecombine, axis=1)
+sundata['sunset'] = sundata.apply(sunsetcombine, axis=1)
+
+print sundata.head()
+print sundata.info()
+
+#drop unnecessary variables from sundata
 sundata = sundata.drop(['month', 'day', 'year'], axis=1)
+
+sundata['date'] = sundata['date'].apply(lambda x: datetime.strftime(x, '%m/%d/%Y'))
 
 #merge the sundata into df
 df = pd.merge(df, sundata, on='date')
 
-#df['sunindicator'] = np.where((((df['sunrise']) - (datetime.timedelta(minutes=30))) < df['time']) & (df['sunset']>df['time']) ,1,0)
-
-#print sundata.describe()
-#print sundata.head()
-print sundata.info()
+#create sunindicator variable which compares sunrise and sunset to time of data and assigns a 1 if the sun is up and a 0 if it is not, with a 30 minute grace period.
+df['sunindicator'] = np.where((((df['sunrise']) - (timedelta(minutes=30))) < df['dateandtime']) & (((df['sunset']) + (timedelta(minutes=30))) > df['dateandtime']) ,1,0)
 
 #print df.mean()
-print df.describe()
+#print df.describe()
 print df.head()
 print df.info()
+
+#drop unnecessary variables from df
+df = df.drop(['sunrise', 'sunset', 'dateandtime', 'date'], axis=1)
 
 #create csv with new variables
 df.to_csv('data/output.csv')
